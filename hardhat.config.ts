8ef929dc5a84abe03728fa6ac9fc6328a766e2d9
franklin-r <alexis.rossi97@gmail.com>
2025-07-03 19:24:32 +0200

    INFRA: Downgrade Solidity compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: {
-		version: "0.8.30",
+		version: "0.8.28",
 		settings: {
 			optimizer: {
 				enabled: true,
